368d8f7a37222dd332122ec33329b1e73a63175b
kingwell47 <joeldoctor47@gmail.com>
2021-12-26 21:20:34 +0800

    implemented vertical line

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CommentCard from "./CommentCard";
+import Grid from "@mui/material/Grid";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepConnector from "@mui/material/StepConnector";
@@ -7,16 +8,19 @@ import StepConnector from "@mui/material/StepConnector";
 const Comment = (props) => {
   return (
     <>
-      <Stepper orientation="vertical">
-        <Step>
-          <CommentCard {...props} />
-        </Step>
-        {props.replies.map((reply) => (
-          <Step key={reply.id} sx={{ pl: 1.5, position: "relative" }}>
-            <CommentCard {...reply} />
-          </Step>
-        ))}
-      </Stepper>
+      <CommentCard {...props} />
+      <Grid container sx={{ mt: 2 }}>
+        <Grid item xs={0.5} sx={{ borderLeft: "1px solid grey" }} />
+        <Grid item xs={11.5}>
+          <Grid container spacing={2}>
+            {props.replies.map((reply) => (
+              <Grid item>
+                <CommentCard {...reply} key={reply.id} />
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 };
