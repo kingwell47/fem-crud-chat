4aa863a154d4330cf6d703f82c9c98696eed99b6
kingwell47 <joeldoctor47@gmail.com>
2021-12-26 21:28:37 +0800

    further improvements to comment component

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -6,21 +6,25 @@ import Step from "@mui/material/Step";
 import StepConnector from "@mui/material/StepConnector";
 
 const Comment = (props) => {
+  const repliesGrid = (
+    <Grid container sx={{ mt: 2 }}>
+      <Grid item xs={0.5} sx={{ borderLeft: "1px solid grey" }} />
+      <Grid item xs={11.5}>
+        <Grid container spacing={2}>
+          {props.replies.map((reply) => (
+            <Grid item key={reply.id}>
+              <CommentCard {...reply} />
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+
   return (
     <>
       <CommentCard {...props} />
-      <Grid container sx={{ mt: 2 }}>
-        <Grid item xs={0.5} sx={{ borderLeft: "1px solid grey" }} />
-        <Grid item xs={11.5}>
-          <Grid container spacing={2}>
-            {props.replies.map((reply) => (
-              <Grid item>
-                <CommentCard {...reply} key={reply.id} />
-              </Grid>
-            ))}
-          </Grid>
-        </Grid>
-      </Grid>
+      {props.replies.length > 0 && repliesGrid}
     </>
   );
 };
