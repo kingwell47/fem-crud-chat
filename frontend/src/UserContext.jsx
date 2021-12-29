bd5bf86ba31f6afe1dec2819018f632f225e5779
kingwell47 <joeldoctor47@gmail.com>
2021-12-29 18:26:44 +0800

    setting up context

---
+++ frontend/src/UserContext.jsx
@@ -0,0 +1,11 @@
+import React, { useState, createContext } from "react";
+
+const UserContext = () => {
+  return (
+    <div>
+      <>Random</>
+    </div>
+  );
+};
+
+export default UserContext;
